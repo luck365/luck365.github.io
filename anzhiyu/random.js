10f002a807a0852bd2957ee31625f7c5358b395a
luck365 <2298148442@qq.com>
2024-06-17 14:54:56 +0800

    Site updated: 2024-06-17 14:54:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/3eeb.html","posts/1014.html","posts/567b.html","posts/f82e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/3eeb.html","posts/f82e.html","posts/1014.html","posts/567b.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
